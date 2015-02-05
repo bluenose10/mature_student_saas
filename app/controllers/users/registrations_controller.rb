198f9f4170bafae2085de88050ad13ba3a819cd3
Mark Moran <mark.moran4@btinternet.com>
2015-02-05 23:24:50 +0000

    Added notice and styles

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -13,10 +13,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
       end 
     end 
   end 
-   
+ 
   private 
    def select_plan 
-    unless params[:plan] && (params[:plan] == 1 || params[:plan] == '2') 
+    unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2') 
       flash[:notice] = "Please select a membership plan to sign up." 
       redirect_to root_url 
     end 
