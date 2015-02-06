9a014a289b0c2a61372f23ce893452c39982ad00
Mark Moran <mark.moran4@btinternet.com>
2015-02-06 19:26:05 +0000

    Made corrections to index and show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,9 +4,9 @@ module UsersHelper
     if @user.profile.job_title == "Student" 
       "<i class='fa fa-graduation-cap'></i>".html_safe 
     elsif @user.profile.job_title == "Tutor" 
-       "<i class='fa fa-university'></i>".html_safe 
+      "<i class='fa fa-university'></i>".html_safe 
     elsif @user.profile.job_title == "Invester" 
-       "<i class='fa fa-money'></i>".html_safe  
+      "<i class='fa fa-money'></i>".html_safe  
     end
   end
 end
